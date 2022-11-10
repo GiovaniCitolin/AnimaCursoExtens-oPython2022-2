4969c6a80119cfa92b715a6df183bbb6ac6cba1a
Giovani Citolin <116692897+GiovaniCitolin@users.noreply.github.com>
2022-11-10 00:36:11 +0000

    Declaração de variaveis (imposto)

---
+++ aula3-2022-11-09b.py
@@ -0,0 +1,25 @@
+#Criação de funções
+
+preco = 1999.90
+
+#Calcular 5% de imposto, guardae na variavel imposto e exibir na tela
+#Calculo
+imposto = preco * 0.05
+#imprime na tela
+print(imposto)
+
+preco2 = 100
+imposto2 = preco2 * 0.05
+print(imposto2)
+
+#Criar uma função calcular imposto que calcula um imposto de 5% e retorna a quem pediu...
+#Declaração da função
+def calcular_imposto(preco_produto):
+  imposto = preco_produto * 0.05
+  return imposto
+
+#Uso da Função para calcular_imposto
+preco = 299
+imposto =calcular_imposto(preco)
+#exibir na tela
+print(imposto)
