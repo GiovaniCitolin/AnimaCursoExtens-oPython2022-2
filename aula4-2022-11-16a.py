f3010aacdc1e1293bf6318470591b293134d9471
Giovani Citolin <116692897+GiovaniCitolin@users.noreply.github.com>
2022-11-16 23:23:39 +0000

    banco de dados

--- aula4-2022-11-16a.py
+++ aula4-2022-11-16a.py
@@ -2,4 +2,18 @@
 import sqlite3
 
 #2º passo: Vamos estabelecer uma conexção com o banco de dados
-sqlite3.connect
+conexao = sqlite3.connect("dc_universe.db")
+
+#3º passo: Criar um objeto do tipo cursosr
+cursor = conexao.cursor()
+
+#4º passo:Comando SQL do banco
+sql = "SELECT pessoa_id, nome, nome_civil, tipo FROM pessoas"
+
+#5º passo: Executar o comando SQÇ no SQLlite (no cursor)
+cursor.execute(sql)
+
+#6º passo: Exibir a consulta com todos os nomes de heróis e vilões do banco de dados
+pessoas = cursor.fetchall()
+for pessoa in pessoas:
+  print(pessoa)
